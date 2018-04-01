181507440c0c54aa95e51f7a6fada2813242e553
CaptainColavin <soleymani.colin@gmail.com>
2018-04-01 15:50:32 +0200

    modify atelier

--- back/offre.php
+++ back/offre.php
@@ -1,70 +1,70 @@
-<?php require_once('base-back.php'); ?>
+<?php require_once('base-back.php');
+include_once('../controllers/workshop.php'); ?>
+<?php /*
+ $ws = new Workshop();
+ $atelier = $ws->workshopList();
+
+ foreach( $atelier as $at ){
+
+   echo('<form>');
+   echo ('<h2>' . $at["workshop_name"].'</h2>');
+
+   echo($at["workshop_infos"]); //TODO parser ce json pour l'afficher proprement
+   echo ('<div class="acc2">3 heures&nbsp;&nbsp;&nbsp;•&nbsp;&nbsp;&nbsp;3 à 6 personnes&nbsp;&nbsp;&nbsp;•&nbsp;&nbsp;&nbsp;chez toi ou tes amis&nbsp;&nbsp;&nbsp;•&nbsp;&nbsp;&nbsp;Paris / région parisienne&nbsp;&nbsp;&nbsp;•&nbsp;&nbsp;&nbsp;tout inclus</div>');
+
+   echo("<p>" . $at["workshop_description"] . "<p>" );
+
+   echo ('</div>');
+ }*/ ?>
 
 
-    <div class="container-fluid">
+         <?php
 
-    <div class="col-md-6">
-      <div class="panel panel-default">
-        <div class="panel-heading">Paramètres de connexion</div>
-        <div class="panel-body">
-          <div class="input-group">
-              <label for="autoLogin" style="margin-right: 10px;">Connexion obligatoire</label>
-              <label class="radio-inline"><input type="radio" id="autoLoginYes" name="autoLogin">Oui</label>
-              <label class="radio-inline"><input type="radio" id="autoLoginNo" name="autoLogin">Non</label>
-          </div>
-          <div class="input-group">
-              <label for="autoLogin" style="margin-right: 10px;">Autorisation utilisateur automatique</label>
-              <label class="radio-inline"><input type="radio" id="autoLoginYes" name="autoLogin">Oui</label>
-              <label class="radio-inline"><input type="radio" id="autoLoginNo" name="autoLogin">Non</label>
-          </div>
-        </div>
-      </div>
-    </div>
+          $ws = new Workshop();
 
-    <div class="col-md-6">
-      <div class="panel panel-default">
-        <div class="panel-heading">Rendu</div>
-        <div class="panel-body">
-          <p class="centered"> C'est quoi ? </p>
-          <p class="centered"> Les seuls ateliers couture chez toi, entre amis, 100% tout inclus ! </p>
-        </div>
-      </div>
-    </div>
 
-    <div class="col-md-6">
-      <div class="panel panel-default">
-        <div class="panel-heading">Personnalisation</div>
-        <div class="panel-body">
-          <p>Images deroulantes</p>
-          <div class="input-group" style="margin-bottom: 10px;">
-            <input type="file" name="picture">
-          </div>
-          <div class="input-group" style="margin-bottom: 10px;">
-            <input type="file" name="picture">
-          </div>
-          <div class="input-group" style="margin-bottom: 10px;">
-            <input type="file" name="picture">
-          </div>
-          <div class="input-group" style="margin-bottom: 10px;">
-            <input type="file" name="picture">
-          </div>
-          <span class="glyphicon glyphicon-plus pull-right" aria-hidden="true"></span>
-          <br/>
-          <hr>
-          <p>C'est quoi ?</p>
-          <div class="input-group" style="margin-bottom: 10px;">
-            <input type="text" class="form-control" placeholder="Descrition 1">
-          </div>
-          <div class="input-group" style="margin-bottom: 10px;">
-            <input type="text" class="form-control" placeholder="Descrition 2">
-          </div>
-          <span class="glyphicon glyphicon-plus pull-right" aria-hidden="true"></span>
-        </div>
-      </div>
-    </div>
+          if(isset($_POST['button'])) {
+              $try = $ws->updateWorkshop(
+                  array(
+                      "id" => $_POST['id'],
+                      "name" =>  $_POST['name'],
+                      "infos" => $_POST['infos'],
+                      "description" => $_POST['desc'],
+                      "price" => $_POST['prix'],
+                      "projects" => $_POST['project']
+                 )
+             );
+          }
+         ?>
 
-	    </div>
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
 
+    <?php
+     $ws = new Workshop();
+     $atelier = $ws->workshopList();
+     $i = 1;
+
+     echo('<h3>Atelier à modifier</h3>');
+     echo('<form class="" action="" method="post">');
+     echo("<select name='id'>");
+     foreach( $atelier as $at ){
+       echo('<option value="'. $i .' ">'. $at["workshop_name"] .'</option>');
+       $i++;
+     }
+     echo ("</select><br>");
+     echo('<label>nom</label><input type="text" name="name" value=""><br>');
+     echo('<label>infos</label><input type="text" name="infos" value=""><br>');
+     echo ('<label>description</label><textarea name="desc" rows="5" cols="30"></textarea><br>');
+     echo('<label>prix</label><input type="number" name="prix" value=""><br>');
+     echo('<label>projets</label><textarea name="project" rows="5" cols="30"></textarea>');
+     echo('<button type="submit" name="button">Modifier la ligne</button>');
+     echo('</form>');
+?>
   </body>
 </html>
-
