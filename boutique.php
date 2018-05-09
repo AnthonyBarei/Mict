bb021447c427bc4c16c662c61edf16c022912e93
Colin Soleymani <soleymani.colin@gmail.com>
2018-05-09 11:23:09 +0200

    début boutique

---
+++ boutique.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+<head>
+	<meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+        <meta name="rights" content="Copyright 2014/2016 - Made in chez toi (FR)"/>
+	<title>Ateliers et cours de couture chez les particuliers • Made in chez toi</title>
+        <meta name="description" content="Ateliers couture collectifs ou cours particuliers, dans ton salon ou chez tes amis :) • Machines et tissus inclus • Pour tous les niveaux !"/>
+        <meta name="keywords" content="couture, cours, lecons, debuter, debutant, commencer, apprendre, facile, paris, region parisienne, 92, 95, 75, 78, 93, 91, 94, atelier, ateliers, domicile, particulier, inclus, lecon, formation, gratuit, amies, amis, debutante, made in chez toi, idf, made, in, chez, toi"/>
+	<link rel="shortcut icon" href="assets/images/favicon.png?v=2" type="image/png" />
+	<link rel="stylesheet" href="assets/css/style_madeincheztoi.css" />
+	<!--<link rel="alternate" type="application/rss+xml" title="Made in chez toi • Actus" href="http://blog.madeincheztoi.com/feed/" />-->
+	<link rel="author" href="http://www.madeincheztoi.com/index.html#faq11" />
+	<link rel="canonical" href="http://www.madeincheztoi.com/" />
+
+  <meta property="og:locale" content="fr_FR" />
+  <meta property="og:type" content="website" />
+  <meta property="og:title" content="Ateliers et cours de couture chez les particuliers • Made in chez toi" />
+  <meta property="og:description" content="Ateliers couture collectifs ou cours particuliers, dans ton salon ou chez tes amis :) • Machines et tissus inclus • Pour tous les niveaux !" />
+  <meta property="og:url" content="http://www.madeincheztoi.com/" />
+  <meta property="og:site_name" content="Made in chez toi" />
+  <meta property="og:image" content="http://www.madeincheztoi.com/assets/images/madeincheztoi-og_thumb.jpg" />
+  <meta name="twitter:card" content="summary"/>
+  <meta name="twitter:title" content="Ateliers et cours de couture chez les particuliers • Made in chez toi"/>
+  <meta name="twitter:description" content="Ateliers couture collectifs ou cours particuliers, dans ton salon ou chez tes amis :) • Machines et tissus inclus • Pour tous les niveaux !"/>
+  <meta name="twitter:domain" content="Made in chez toi"/>
+  <meta name="twitter:image:src" content="http://www.madeincheztoi.com/assets/images/madeincheztoi-og_thumb.jpg"/>
+</head>
+
+  <body >
+      <nav class="nav_menu">
+      <input type="checkbox" class="menu-toggler" id="menu-toggler" aria-hidden="true">
+                  <label for="menu-toggler" class="menu-toggler__label" aria-hidden="true">
+
+      <div id="menu_call"><img src="assets/images/bottom.png" alt=""/> MENU</div>
+   </label>
+      <ul id="menutop" class="menu">
+      <li><a class="scrollTo" href="index.php#home" title="Début">Home</a></li>
+      <li><a class="scrollTo" href="index.php#quoi" title="C'est quoi ?">C'est quoi ?</a></li>
+      <li><a class="scrollTo" href="index.php#offres" title="Les offres Made in chez toi">Offres</a></li>
+      <li><a href="ateliers_et_cours_de_couture_a_domicile.php" title="Le détail des ateliers">Ateliers</a></li>
+      <li><a class="scrollTo" href="index.php#contact" title="Je veux contacter Made in chez toi">Contact</a></li>
+      <li><a href="photos.html" title="Galerie photos">Photos</a></li>
+      <li><a class="scrollTo" href="index.php#faq" title="Toutes les réponses sont là !">FÀQ-propos</a></li>
+      <li><a href="boutique.php" title="Boutique">Boutique</a></li>
+      <li><a class="scrollTo" href="index.php#partenaires" title="Plus on est de fous...">Partenaires</a></li>
+      <li><a href="http://blog.madeincheztoi.com/" title="Les actus en temps réel !">Blog</a></li>
+      </ul>
+    </nav>
+
+  <div class="wrapper row">
+    <div class="col-md-3">
+      <iframe src="https://www.payfacile.com/made-in-chez-toi/si/carte-cadeau-test" height="680" frameborder="1"></iframe>
+    </div>
+    <div class="col-md-3">
+      <iframe src="https://www.payfacile.com/made-in-chez-toi/si/carte-cadeau-test" height="680" frameborder="1"></iframe>
+    </div>
+  </div>
+
+  </body>
+</html>
