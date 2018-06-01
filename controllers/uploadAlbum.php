3e0b36e184c378f7d80eba690c0ecbde3121830d
Colin Soleymani <soleymani.colin@gmail.com>
2018-06-01 14:40:14 +0200

    correction redirect

--- controllers/uploadAlbum.php
+++ controllers/uploadAlbum.php
@@ -7,40 +7,40 @@ $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
 if(isset($_POST["submit"])) {
     $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
     if($check !== false) {
-        echo "File is an image - " . $check["mime"] . ".</br>";
+        //echo "File is an image - " . $check["mime"] . ".</br>";
         $uploadOk = 1;
     } else {
-        echo "File is not an image.</br>";
+        //echo "File is not an image.</br>";
         $uploadOk = 0;
     }
 }
 // Check if file already exists
 if (file_exists($target_file)) {
-    echo "Sorry, file already exists.</br>";
+    //echo "Sorry, file already exists.</br>";
     $uploadOk = 0;
 }
 // Check file size
 if ($_FILES["fileToUpload"]["size"] > 5000000) {
-    echo "Sorry, your file is too large.</br>";
+    //echo "Sorry, your file is too large.</br>";
     $uploadOk = 0;
 }
 // Allow certain file formats
 if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
 && $imageFileType != "gif" ) {
-    echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.</br>";
+    //echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.</br>";
     $uploadOk = 0;
 }
 // Check if $uploadOk is set to 0 by an error
 if ($uploadOk == 0) {
-    echo "Sorry, your file was not uploaded.</br>";
+    //echo "Sorry, your file was not uploaded.</br>";
 // if everything is ok, try to upload file
 } else {
     if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
-        echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.</br>";
+        //echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.</br>";
     } else {
-        echo "Sorry, there was an error uploading your file.</br>";
+        //echo "Sorry, there was an error uploading your file.</br>";
     }
 }
-header("Location: " . $_POST['redirect']);
+header("Location: ".$_POST['redirect']);
 die();
 ?>
